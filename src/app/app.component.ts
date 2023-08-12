f9bde1309aebc2ffac5d4a7e60c56401eb4134d7
= <=>
2023-08-12 13:31:11 +0400

    add service api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'crocobet-assignment';
+export class AppComponent implements OnInit {
+  users: any[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getUsers().subscribe((users) => {
+      this.users = users;
+      console.log(this.users);
+    });
+  }
 }
